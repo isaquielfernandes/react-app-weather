c4bc31cf5930123ae959a5dd6e895f74e5e3f80a
IF <iisaquielfernandes@gmail.com>
2020-10-26 17:37:48 +0000

    Update useFetchWeather.js

--- src/service/useFetchWeather.js
+++ src/service/useFetchWeather.js
@@ -1,51 +1,13 @@
-import { useReducer, useEffect } from 'react'
-import aios from 'aixos'
+const baseUrl = 'https://api.openweathermap.org/data/2.5';
 
-const API_URL = ''
+export const fetchWeatherData = (city) => {
+  let url = `${baseUrl}/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
 
-const ACTIONS = {
-    GET_DATA: 'get-data',
-    MAKE_REQUEST: 'make-request',
-    ERROR: 'error'
-}
+  return fetch(url);
+};
 
-function reducer(state, action) {
-    switch(action.type){
-        case ACTIONS.MAKE_REQUEST:
-            return { loading: true, data: [] }
-        case ACTIONS.GET_DATA:
-            return { ...state, loading: false, data: action.payload.data }
-        case ACTIONS.ERROR:
-            return { ...state, loading: false, error: action.payload.error, data: []}
-        default:
-            return state
-    }
-}
+export const fetchExtendedForecastData = (city) => {
+  let url = `${baseUrl}/forecast/daily?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
 
-const useFetchWeather = (params) => {
-
-    const [state, dispatch] = useReducer(reducer, {data: [], loading: true})
-
-    useEffect(() => {
-        const cancelToken = axios.CancelToken.source()
-        dispatch({type: ACTIONS.MAKE_REQUEST})
-        axios.get(BASE_URL, {
-            cancelToken: cancelToken.token,
-            params: {...params}
-        }).then(res => {
-            dispatch({type: ACTIONS.GET_DATA, payload: {data: res.data}})
-        }).catch(e => {
-            if(axios.isCancel(e)) return
-            dispatch({type: ACTIONS.ERROR, payload: {error: e}})
-        })
-
-        return () => {
-            cancelToken.cancel()
-        }
-
-    }, [params])
-
-    return state
-}
-
-export default useFetchWeather
+  return fetch(url);
+};
