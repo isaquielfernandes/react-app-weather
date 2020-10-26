6e5ca6b2af44022825136115c6801fac0620a6df
IF <iisaquielfernandes@gmail.com>
2020-10-26 22:11:06 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { store, persisetdStore } from './store/store'
+
 ReactDOM.render(
     <Router>
-      <App />
+      <Provider store={store}>
+          <PersistGate persistor={persisetdStore}>
+             <App />
+          </PersistGate>
+       </Provider>
     </Router>,
   document.getElementById('root')
 );
