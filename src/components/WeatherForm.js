c4972febbee81c488cb02330c59d150a7c30539b
IF <iisaquielfernandes@gmail.com>
2020-10-27 20:26:46 +0000

    Update WeatherForm.js

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Card, Form } from 'react-bootstrap'
+import { Card, Form, Button } from 'react-bootstrap'
 
-const WeatherForm = props => (
+const WeatherForm = (props) => (
 
     <Card.Body className="card">
         <Form onSubmit={props.getWeather}>
             <div className="form-group">
                 <input type="text" name="city" placeholder="Your City Name" className="form-control" autoFocus/>
             </div>
-            <button className="btn btn-info btn-block">
+            <Button variant="info" className="btn-block">
                 Get Weather
-            </button>
+            </Button>
         </Form>
     </Card.Body>
 );
