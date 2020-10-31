6de4370cb051b5b867b06b77d1a610ca9118d4fd
IF <iisaquielfernandes@gmail.com>
2020-10-31 20:36:38 +0000

    Update ForecastContainer.js

--- src/components/forecast/ForecastContainer.js
+++ src/components/forecast/ForecastContainer.js
@@ -9,21 +9,21 @@ const ForecastContainer = () => {
   return (
     <Row className="mt-2">
       <Col>
-        <Card className="daily-container">
+        <div className="daily-container">
           <Card.Title>
             <h3 data-v-3724b8e4="" className="text-center pt-2">
               Forecast
             </h3>
             <hr />
           </Card.Title>
-          <Card.Body className="animated fadeInUp day-list">
+          <Card.Body className="animated fadeInUp p-0 day-list">
             <Accordion >
             {forecast.daily.map((daily) => (
               <ForecastItem key={daily.dt} daily={daily} />
             ))}
             </Accordion>
           </Card.Body>
-        </Card>
+        </div>
       </Col>
     </Row>
   );
