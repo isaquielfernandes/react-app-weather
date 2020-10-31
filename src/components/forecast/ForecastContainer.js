89e1ea4bc08a0a76a4c4c70dd7468327afc3d137
IF <iisaquielfernandes@gmail.com>
2020-10-31 19:40:57 +0000

    Update ForecastContainer.js

--- src/components/forecast/ForecastContainer.js
+++ src/components/forecast/ForecastContainer.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import ForecastItem from './ForecastItem'
+import React from "react";
+import { Card, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import ForecastItem from "./ForecastItem";
 
 const ForecastContainer = () => {
-
-    const  { forecast } = useSelector(state => state.forecast)
-    
-    return (
+  const { forecast } = useSelector((state) => state.forecast);
+  console.log(forecast.daily);
+  return (
+    <Row className="mt-2">
+      <Col>
         <Card className="daily-container">
-            <Card.Title>
-                <h3 data-v-3724b8e4="" className="text-center pt-2">Forecast</h3>
-                <hr/>
-            </Card.Title>
-            <Card.Body className="animated fadeInUp day-list">
-                {
-                    forecast.daily.map(daily => <ForecastItem key={daily.dt} daily={daily}/>)
-                }
-            </Card.Body>
+          <Card.Title>
+            <h3 data-v-3724b8e4="" className="text-center pt-2">
+              Forecast
+            </h3>
+            <hr />
+          </Card.Title>
+          <Card.Body className="animated fadeInUp day-list">
+            {forecast.daily.map((daily) => (
+              <ForecastItem key={daily.dt} daily={daily} />
+            ))}
+          </Card.Body>
         </Card>
-    );
+      </Col>
+    </Row>
+  );
 };
 
 export default ForecastContainer;
