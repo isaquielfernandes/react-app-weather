57832ee428a4ba0cc4eccbf7ba44d1dcd14e0ea8
IF <iisaquielfernandes@gmail.com>
2020-10-31 19:35:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React from "react"
-//import { useSelector, useDispatch} from 'react-redux'
+import React from "react";
 import "./App.css";
-import { Container } from "react-bootstrap"
-import NavBar from "./components/NavBar"
-import Search from "./components/Search"
-import Weather from "./components/Weather"
-import ForecastContainer from './components/forecast/ForecastContainer'
-
+import { Container } from "react-bootstrap";
+import NavBar from "./components/NavBar";
+import Search from "./components/Search";
+import Weather from "./components/Weather";
+import ForecastContainer from "./components/forecast/ForecastContainer";
 
 const App = () => {
-
   return (
     <>
       <NavBar />
       <Container className="p-4">
+        <Search />
         <div className="row">
           <div className="col-md-4">
-            <Search />
             <Weather />
           </div>
-          <div className="col-md-8">
-            <ForecastContainer />
-          </div>
         </div>
+        <ForecastContainer />
       </Container>
     </>
   );
