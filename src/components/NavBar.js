75e760626460c4f5cb0c6f358fdd079606089536
IF <iisaquielfernandes@gmail.com>
2020-11-01 10:25:46 +0000

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import { Navbar, Nav } from "react-bootstrap";
 
 const NavBar = () => (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Weather</Navbar.Brand>
+            <Navbar.Brand href="#home">
+              <img
+                alt="logo"
+                src="/logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                />{' '}
+              Weather
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
