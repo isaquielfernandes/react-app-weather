7d69445751c438505f71eaae404886ba1ba4c9ef
IF <iisaquielfernandes@gmail.com>
2020-11-08 18:24:25 +0000

    Update useFetchWeather.js

--- src/api/useFetchWeather.js
+++ src/api/useFetchWeather.js
@@ -1,6 +1,5 @@
 const baseUrl = 'https://api.openweathermap.org/data/2.5';
 
-
 export const fetchWeatherData = (city) => {
   let url = `${baseUrl}/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`;
   return fetch(url);
