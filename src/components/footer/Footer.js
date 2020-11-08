5e2b52aa07541e9beae95fb1199e685a70abb3b8
IF <iisaquielfernandes@gmail.com>
2020-11-08 21:00:35 +0000

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <footer className="footer p3">
+    <footer className="footer p-3 mb-1">
       <hr/>
       {'Copyright © '}
       <span className="p-1 m-1 text-center">By Isaquiel Fernandes</span>
