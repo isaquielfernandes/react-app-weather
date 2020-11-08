154a1bce099a3a10043abb56764b7a96abcfc4f4
IF <iisaquielfernandes@gmail.com>
2020-11-08 19:47:44 +0000

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="">
-      <span></span>
-    </div>
+    <footer className="">
+      <hr/>
+      <span>By Isaquiel Fernandes</span>
+    </footer>
   )
 };
