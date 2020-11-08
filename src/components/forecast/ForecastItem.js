37fb224daf82b33d33c7d21958e4720e224c5e41
IF <iisaquielfernandes@gmail.com>
2020-11-08 18:46:57 +0000

    Update ForecastItem.js

--- src/components/forecast/ForecastItem.js
+++ src/components/forecast/ForecastItem.js
@@ -8,7 +8,7 @@ const ForecastItem = ({ daily, variant }) => {
     <Card.Body 
        bg={variant.toLowerCase()}
        text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-       className="mb-2">
+       className="mb-2 p-0">
       <Accordion.Toggle
         as={Card.Header}
         variant="link"
