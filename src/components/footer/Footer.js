d7873f3a57032d8430c02c20769a0b7950e98aea
IF <iisaquielfernandes@gmail.com>
2020-11-08 19:52:54 +0000

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <footer className="">
       <hr/>
-      <span>By Isaquiel Fernandes</span>
+      <span className="text-center">By Isaquiel Fernandes</span>
     </footer>
   )
 };
+
+export default Footer;
