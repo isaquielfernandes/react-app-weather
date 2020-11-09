d5919f67da3e5a8f554d53881273bb2ff27a6d28
IF <iisaquielfernandes@gmail.com>
2020-11-09 15:17:54 +0000

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,12 +3,14 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <footer className="footer p-3 mb-1">
+    <footer className="footer">
       <hr/>
-      {'Copyright © '}
-      <span className="py-1 my-1">By Isaquiel Fernandes</span>
-      {new Date().getFullYear()}
-      {'.'}
+      <div className="p-3 mt-2">
+        {'Copyright © '}
+        <span className="py-1 my-1">By Isaquiel Fernandes</span>
+        {new Date().getFullYear()}
+        {'.'}
+      </div>
     </footer>
   )
 };
