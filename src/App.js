cf7b17ee9434063593772b0af04721fcc5c8a619
IF <iisaquielfernandes@gmail.com>
2020-11-09 15:46:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div style={{ minHeight: "100vh" }}>
       <NavBar />
-      <Container style={{ minHeight: "70%" }}>
+      <Container style={{ minHeight: "60vh" }}>
         <Search />
         <Weather />
         <ForecastContainer />
