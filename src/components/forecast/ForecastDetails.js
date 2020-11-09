c2b4e7b2ec7b86dedd920509c22fc8f5c8db09b0
IF <iisaquielfernandes@gmail.com>
2020-11-09 00:47:35 +0000

    Update ForecastDetails.js

--- src/components/forecast/ForecastDetails.js
+++ src/components/forecast/ForecastDetails.js
@@ -135,7 +135,7 @@ const ForecastDetails = ({ daily }) => {
             </span>
           </li>
         </ul>
-        <table data-v-1ab7fc2c className="table table-sm">
+        <table data-v-1ab7fc2c className="">
           <tbody>
             <tr data-v-1ab7fc2c>
               <th data-v-1ab7fc2c />
